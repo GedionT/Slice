ee01f4860e567af40beba637be2349e1e6f8caee
GedionT <teshomegedion@gmail.com>
2021-08-02 16:48:56 +0300

    added cors to all routes

--- Backend/entrypoint.js
+++ Backend/entrypoint.js
@@ -1,58 +1,61 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import Routes from './routes/routes';
-import Error from './app/Exceptions/error';
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import Routes from "./routes/routes";
+import Error from "./app/Exceptions/error";
+import cors from "cors";
 
-const RateLimit = require('express-rate-limit');
+const RateLimit = require("express-rate-limit");
 // set up rate limiter: max 5 request per min per ip
 var limiter = new RateLimit({
-  windowMs: 1*60*1000, // 1 minute
-  max: 5
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 5,
 });
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 app.use(limiter);
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
   next();
-}); 
+});
 
-app.use('/api/users', Routes.AccountApiRouter); 
+app.use("/api/users", Routes.AccountApiRouter);
 
 app.use((req, res, next) => {
-  const error = new Error('Could not find this route.', 404);      
+  const error = new Error("Could not find this route.", 404);
   throw error;
 });
 
-app.use((error, req, res, next) => {         
+app.use((error, req, res, next) => {
   if (res.headerSent) {
     return next(error);
   }
-  res.status(error.code || 500)
-  res.json({message: error.message || 'An unknown error occurred!', success: error.success||false});
+  res.status(error.code || 500);
+  res.json({
+    message: error.message || "An unknown error occurred!",
+    success: error.success || false,
+  });
 });
 
 mongoose
   .connect(
     `mongodb+srv://${process.env.name}:${process.env.password}@cluster0.kzetf.mongodb.net/${process.env.db}?retryWrites=true&w=majority`,
-    { useNewUrlParser: true, useUnifiedTopology: true,useCreateIndex: true }
-    
+    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
   )
   .then(() => {
-    console.log('listening at port',process.env.PORT || 5000 )
+    console.log("listening at port", process.env.PORT || 5000);
     app.listen(process.env.PORT || 5000);
   })
-  .catch(err => {
+  .catch((err) => {
     console.log(err);
   });
-  
