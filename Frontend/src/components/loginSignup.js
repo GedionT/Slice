70c5e931cdf9ae9b9ad2b0da16cb97a145a24379
Hargun kaur <56452820+hkaur008@users.noreply.github.com>
2021-08-04 23:59:02 +0000

    fortend setup

--- Frontend/src/components/loginSignup.js
+++ Frontend/src/components/loginSignup.js
@@ -14,8 +14,8 @@ import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
-import { useHistory } from 'react-router-dom';
 
+import { useHistory } from 'react-router-dom';
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
